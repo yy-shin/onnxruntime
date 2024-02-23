def5264417eb176bb04792db80c129cc5f204d7b
Changming Sun <chasun@microsoft.com>
2024-02-23 14:34:57 -0800

    update

--- tools/ci_build/github/linux/docker/inference/x64/default/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/x64/default/cpu/scripts/install_centos.sh
@@ -5,5 +5,5 @@ os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
 
 echo "installing for CentOS version : $os_major_version"
 rpm -Uvh https://packages.microsoft.com/config/centos/$os_major_version/packages-microsoft-prod.rpm
-dnf install -y python39-devel glibc-langpack-\* glibc-locale-source which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel msopenjdk-11 graphviz gcc-toolset-13-binutils gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran gcc-toolset-13-libasan-devel
+dnf install -y python39-devel glibc-langpack-\* glibc-locale-source which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel msopenjdk-11 graphviz gcc-toolset-13-binutils gcc-toolset-13-gcc gcc-toolset-13-gcc-c++
 locale
