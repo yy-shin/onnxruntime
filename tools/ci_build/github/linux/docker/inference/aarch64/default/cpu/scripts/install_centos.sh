9a2a005525e2865db2d71ca5034408657927b33b
Changming Sun <chasun@microsoft.com>
2024-02-24 11:41:22 -0800

    update

--- tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
@@ -2,7 +2,12 @@
 set -e -x
 
 os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
-
 echo "installing for CentOS version : $os_major_version"
-dnf install -y python39-devel glibc-langpack-\* glibc-locale-source which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel java-11-openjdk-devel graphviz gcc-toolset-12-binutils gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ gcc-toolset-12-gcc-gfortran
+dnf config-manager --set-disabled "ubi-$os_major_version-codeready-builder-rpms"
+dnf config-manager  --save --setopt ubi-8-appstream-rpms.exclude=dotnet*,aspnetcore*,netstandard*
+rpm -Uvh https://packages.microsoft.com/config/centos/$os_major_version/packages-microsoft-prod.rpm
+dnf install -y python3.11-devel glibc-langpack-\* glibc-locale-source which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel msopenjdk-11 graphviz gcc-toolset-12-binutils gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ gcc-toolset-12-libasan-devel dotnet-sdk-8.0 git
 locale
+dotnet --list-sdks
+#For running C# tests
+dotnet workload install android wasm-tools-net6
