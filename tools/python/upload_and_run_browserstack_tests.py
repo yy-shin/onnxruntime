e0f155a95d1eeffbb7ed4a3fb5ac65d1f1333e02
carzh <wolfivyaura@gmail.com>
2024-09-23 10:47:31 -0700

    sync with pr branch

--- tools/python/upload_and_run_browserstack_tests.py
+++ tools/python/upload_and_run_browserstack_tests.py
@@ -1,71 +1,121 @@
-import json
 import argparse
+import os
 import time
+
 import requests
 
-parser = argparse.ArgumentParser("Upload and run BrowserStack tests")
 
-parser.add_argument("--id", type=str, help="BrowserStack user ID")
-parser.add_argument("--token", type=str, help="BrowserStack token")
-parser.add_argument("--app_apk_path", type=str, help="Path to the app APK")
-parser.add_argument("--test_apk_path", type=str, help="Path to the test suite APK")
-# TODO: add link to browserstack documentation of available device strings you can pass in
-parser.add_argument("--devices", type=str, nargs="+", help="List of devices to run the tests on")
-
-args = parser.parse_args()
+def response_to_json(response):
+    response.raise_for_status()
+    response_json = response.json()
+    print(response_json)
+    print("-" * 30)
+    return response_json
 
 
-def post_response_to_json(response):
-    if len(response) == 0:
-        print("response received: ", response)
-        raise Exception("No response from BrowserStack")
-    try:
-        json_response = json.loads(response)
-        print("JSON response: ", json.dumps(json_response, indent=2))
-        if "error" in json_response:
-            raise Exception(json_response["error"])
-        return json_response
-    except:
-        print("response received: ", response)
-        raise Exception("Invalid JSON response from BrowserStack")
+def upload_apk_parse_json(post_url, apk_path, id, token):
+    with open(apk_path, "rb") as apk_file:
+        response = requests.post(post_url, files={"file": apk_file}, auth=(id, token))
+    return response_to_json(response)
 
-def upload_apk_parse_json(post_url, apk_path):
-    response = requests.post(post_url, files={"file": open(apk_path, "rb")}, auth=(args.id, args.token))
-    return post_response_to_json(response.text)
 
-upload_app_json = upload_apk_parse_json("https://api-cloud.browserstack.com/app-automate/espresso/v2/app", args.app_apk_path)
-upload_test_json = upload_apk_parse_json("https://api-cloud.browserstack.com/app-automate/espresso/v2/test-suite", args.test_apk_path)
+def browserstack_build_request(devices, app_url, test_suite_url, test_platform, id, token):
+    headers = {}
 
-headers = {}
+    json_data = {
+        "devices": devices,
+        "app": app_url,
+        "testSuite": test_suite_url,
+    }
 
-json_data = {
-    'devices': [
-        'Samsung Galaxy S22-12.0',
-    ],
-    'app': upload_app_json["app_url"],
-    'testSuite': upload_test_json["test_suite_url"],
-}
-
-build_response = requests.post(
-    'https://api-cloud.browserstack.com/app-automate/espresso/v2/build',
-    headers=headers,
-    json=json_data,
-    auth=(args.id, args.token)
-)
-
-build_response_json = post_response_to_json(build_response.text)
-
-# GET TEST RESULTS
-
-tests_status = "running"
-
-while tests_status == "running":
-    time.sleep(30)
-    test_response = requests.get(
-        'https://api-cloud.browserstack.com/app-automate/espresso/v2/builds/{build_id}'.format(build_id = build_response_json["build_id"]),
-        auth=(args.id, args.token)
+    build_response = requests.post(
+        f"https://api-cloud.browserstack.com/app-automate/{test_platform}/v2/build",
+        headers=headers,
+        json=json_data,
+        auth=(id, token),
     )
 
-    test_response_json = post_response_to_json(test_response.text)
+    return response_to_json(build_response)
 
-    tests_status = test_response_json["status"]
+
+def build_query_loop(build_id, test_platform, id, token):
+    """Called after a build is initiated on Browserstack. Repeatedly queries the REST endpoint to check for the
+    status of the tests in 30 second intervals.
+    """
+    tests_status = "running"
+
+    while tests_status == "running":
+        time.sleep(30)
+
+        test_response = requests.get(
+            f"https://api-cloud.browserstack.com/app-automate/{test_platform}/v2/builds/{build_id}",
+            auth=(id, token),
+        )
+
+        test_response_json = response_to_json(test_response)
+        tests_status = test_response_json["status"]
+
+    return tests_status
+
+
+if __name__ == "__main__":
+    # handle cli args
+    parser = argparse.ArgumentParser("Upload and run BrowserStack tests")
+
+    parser.add_argument(
+        "--test_platform", type=str, help="Testing platform", choices=["espresso", "xcuitest"], required=True
+    )
+    parser.add_argument("--app_apk_path", type=str, help="Path to the app APK", required=True)
+    parser.add_argument("--test_apk_path", type=str, help="Path to the test suite APK", required=True)
+    parser.add_argument(
+        "--devices",
+        type=str,
+        nargs="+",
+        help="List of devices to run the tests on. For more info, "
+        "see https://www.browserstack.com/docs/app-automate/espresso/specify-devices",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    browserstack_id = os.environ["BROWSERSTACK_ID"]
+    browserstack_token = os.environ["BROWSERSTACK_TOKEN"]
+
+    # Upload the app and test suites
+    upload_app_json = upload_apk_parse_json(
+        f"https://api-cloud.browserstack.com/app-automate/{args.test_platform}/v2/app",
+        args.app_apk_path,
+        browserstack_id,
+        browserstack_token,
+    )
+    upload_test_json = upload_apk_parse_json(
+        f"https://api-cloud.browserstack.com/app-automate/{args.test_platform}/v2/test-suite",
+        args.test_apk_path,
+        browserstack_id,
+        browserstack_token,
+    )
+
+    # Initiate build (send request to run the tests)
+    build_response_json = browserstack_build_request(
+        args.devices,
+        upload_app_json["app_url"],
+        upload_test_json["test_suite_url"],
+        args.test_platform,
+        browserstack_id,
+        browserstack_token,
+    )
+
+    # Get build status until the tests are no longer running
+    tests_status = build_query_loop(
+        build_response_json["build_id"], args.test_platform, browserstack_id, browserstack_token
+    )
+
+    test_suite_details_url = (
+        f"https://app-automate.browserstack.com/dashboard/v2/builds/{build_response_json['build_id']}"
+    )
+
+    print("=" * 30)
+    print("Test suite details: ", test_suite_details_url)
+    print("=" * 30)
+    if tests_status != "passed":
+        raise Exception(f"Tests failed. Go to {test_suite_details_url} for more details.")
