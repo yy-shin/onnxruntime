83c67210d09ed7b226553d8a6bf3942a5b46badd
Kyle Zhang <zhanxi@microsoft.com>
2024-09-23 13:58:56 +0800

    debug

--- tools/ci_build/github/azure-pipelines/templates/0-kyle-tester.ps1
+++ tools/ci_build/github/azure-pipelines/templates/0-kyle-tester.ps1
@@ -4,16 +4,15 @@ $GPG_PATH = "C:\Program Files (x86)\gnupg\bin\gpg.exe"
 Start-Process -FilePath $GPG_PATH -ArgumentList "--version" -NoNewWindow -PassThru -Wait
 Write-Host "================GPG VERSION END================="
 
-
 Write-Host "================PARAM EXISTED START================="
-if (${{ java-pgp-pwd }}) {
+if (${env:java-pgp-pwd}) {
     Write-Host "YES! java-pgp-pwd exists"
 }
 else {
     Write-Host "NO! java-pgp-pwd NOT exists"
 }
 
-if (${{ java-pgp-key }}) {
+if (${env:java-pgp-key}) {
     Write-Host "YES! java-pgp-key exists"
 }
 else {
