e2206f5c3d63358dceffac751903a1a5f8d0122d
Kyle Zhang <zhanxi@microsoft.com>
2024-09-23 14:53:44 +0800

    debug

--- tools/ci_build/github/azure-pipelines/templates/0-kyle-tester.ps1
+++ tools/ci_build/github/azure-pipelines/templates/0-kyle-tester.ps1
@@ -4,20 +4,38 @@ $GPG_PATH = "C:\Program Files (x86)\gnupg\bin\gpg.exe"
 Start-Process -FilePath $GPG_PATH -ArgumentList "--version" -NoNewWindow -PassThru -Wait
 Write-Host "================GPG VERSION END================="
 
-Write-Host "================PARAM EXISTED START================="
+Write-Host "================PARAM EXISTED START - env:XXX ================="
 if (${env:java-pgp-pwd}) {
-    Write-Host "YES! java-pgp-pwd exists"
+    Write-Host "YES! java-pgp-pwd exists - env:XXX "
 }
 else {
-    Write-Host "NO! java-pgp-pwd NOT exists"
+    Write-Host "NO! java-pgp-pwd NOT exists - env:XXX "
 }
 
 if (${env:java-pgp-key}) {
-    Write-Host "YES! java-pgp-key exists"
+    Write-Host "YES! java-pgp-key exists - env:XXX "
 }
 else {
-    Write-Host "NO! java-pgp-key NOT exists"
+    Write-Host "NO! java-pgp-key NOT exists - env:XXX "
 }
 
-Write-Host "================PARAM EXISTED START================="
+Write-Host "================PARAM EXISTED START - env:XXX ================="
+
+Write-Host "================PARAM EXISTED START - parameters.XXX ================="
+if (${{ parameters.java-pgp-pwd }}) {
+    Write-Host "YES! java-pgp-pwd exists - parameters.XXX "
+}
+else {
+    Write-Host "NO! java-pgp-pwd NOT exists - parameters.XXX "
+}
+
+if (${{ parameters.java-pgp-key }}) {
+    Write-Host "YES! java-pgp-key exists - parameters.XXX "
+}
+else {
+    Write-Host "NO! java-pgp-key NOT exists - parameters.XXX "
+}
+
+Write-Host "================PARAM EXISTED START - parameters.XXX ================="
+
 exit 1
