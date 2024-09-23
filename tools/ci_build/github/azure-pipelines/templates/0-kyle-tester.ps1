0ba02c81fa4d0bf0511f931f28603cf27fd625ce
Kyle Zhang <zhanxi@microsoft.com>
2024-09-23 18:07:07 +0800

    debug

--- tools/ci_build/github/azure-pipelines/templates/0-kyle-tester.ps1
+++ tools/ci_build/github/azure-pipelines/templates/0-kyle-tester.ps1
@@ -3,30 +3,74 @@ $working_directory = $Args[0]
 $jar_file_directory = $working_directory + "\debugging_target_folder"
 New-Item -ItemType "directory" -Path $jar_file_directory
 
-$gpg_path = "C:\Program Files (x86)\gnupg\bin\gpg.exe"
-$passphrase_file = Join-Path -Path $working_directory -ChildPath "passphrase.txt"
-$private_key_file = Join-Path -Path $working_directory -ChildPath "private_key.txt"
-
-Write-Host "Generating passphrase and private key files."
-Out-File -FilePath $passphrase_file -InputObject $(java-pgp-pwd)
-Out-File -FilePath $private_key_file -InputObject $(java-pgp-key)
-Write-Host "Generated passphrase and private key files."
-
 Write-Host "Generating fake jar files."
 Out-File -FilePath $jar_file_directory"\test1.jar" -InputObject "this is a test1 jar file."
 Write-Host "Generated fake jar files."
 
-Write-Host "Importing private key file."
-$import_key_args_list = "--batch --import `"$private_key_file`""
-Start-Process -FilePath $GPG_PATH -ArgumentList $import_key_args_list -NoNewWindow -PassThru -Wait
-Write-Host "Imported private key file."
+$gpg_path = "C:\Program Files (x86)\gnupg\bin\gpg.exe"
+$passphrase_file = Join-Path -Path $working_directory -ChildPath "passphrase.txt"
+$private_key_file = Join-Path -Path $working_directory -ChildPath "private_key.txt"
 
-$targeting_original_files = Get-ChildItem $jar_file_directory -Recurse -Force -File -Name
-foreach ($file in $targeting_original_files) {
-    $file_path = Join-Path $jar_file_directory -ChildPath $file
-    Write-Host "GPG signing to file: "$file_path
-    $args_list = "--pinentry-mode loopback --passphrase-file `"$passphrase_file`" -ab `"$file_path`""
-    Start-Process -FilePath $GPG_PATH -ArgumentList $args_list -NoNewWindow -PassThru -Wait
+
+#format 1
+try {
+    Write-Host "Generating passphrase and private key files."
+    Out-File -FilePath $passphrase_file -InputObject "$(java-pgp-pwd)"
+    Write-Host "Generated passphrase and private key files."
+
+    Write-Host "Importing private key file."
+    $import_key_args_list = "--batch --import `"$private_key_file`""
+    Start-Process -FilePath $GPG_PATH -ArgumentList $import_key_args_list -NoNewWindow -PassThru -Wait
+    Write-Host "Imported private key file."
+}
+catch {
+    Write-Host "FAILED: format 1"
+}
+
+
+#format 2
+try {
+    Write-Host "Generating passphrase and private key files."
+    Out-File -FilePath $passphrase_file -InputObject '$(java-pgp-pwd)'
+    Write-Host "Generated passphrase and private key files."
+
+    Write-Host "Importing private key file."
+    $import_key_args_list = "--batch --import `"$private_key_file`""
+    Start-Process -FilePath $GPG_PATH -ArgumentList $import_key_args_list -NoNewWindow -PassThru -Wait
+    Write-Host "Imported private key file."
+}
+catch {
+    Write-Host "FAILED: format 2"
+}
+
+#format 3
+try {
+    Write-Host "Generating passphrase and private key files."
+    Out-File -FilePath $passphrase_file -InputObject `'$(java-pgp-pwd)`'
+    Write-Host "Generated passphrase and private key files."
+
+    Write-Host "Importing private key file."
+    $import_key_args_list = "--batch --import `"$private_key_file`""
+    Start-Process -FilePath $GPG_PATH -ArgumentList $import_key_args_list -NoNewWindow -PassThru -Wait
+    Write-Host "Imported private key file."
+}
+catch {
+    Write-Host "FAILED: format 3"
+}
+
+#format 4
+try {
+    Write-Host "Generating passphrase and private key files."
+    Out-File -FilePath $passphrase_file -InputObject `"$(java-pgp-pwd)`"
+    Write-Host "Generated passphrase and private key files."
+
+    Write-Host "Importing private key file."
+    $import_key_args_list = "--batch --import `"$private_key_file`""
+    Start-Process -FilePath $GPG_PATH -ArgumentList $import_key_args_list -NoNewWindow -PassThru -Wait
+    Write-Host "Imported private key file."
+}
+catch {
+    Write-Host "FAILED: format 4"
 }
 
 $targeting_asc_files = Get-ChildItem $jar_file_directory -Recurse -Force -File -Name
